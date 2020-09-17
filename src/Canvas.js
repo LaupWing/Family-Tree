3a692844572310dffa893672f8a9c6cfb4d12624
LaupWing <loc_strict@hotmail.com>
2020-09-17 23:23:22 +0200

    draw func

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,13 +1,19 @@
-import React, {useRef} from 'react';
+import React, {useRef, useEffect} from 'react';
 
 const Canvas = props =>{
     const canvasRef = useRef(null);
+
+    const draw = ctx => {
+        ctx.fillStyle = '#000000'
+        ctx.beginPath()
+        ctx.arc(50, 100, 20, 0, 2*Math.PI)
+        ctx.fill()
+      }
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
         //Our first draw
-        context.fillStyle = '#000000'
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+        draw(context);
     }, [])
     return <canvas ref={canvasRef} {...props}/>
 }
