fb3f88df52f49fe0f831ef791f434f0cebbaddb9
LaupWing <loc_strict@hotmail.com>
2020-09-17 23:39:31 +0200

    draw

--- src/Canvas.js
+++ src/Canvas.js
@@ -4,14 +4,14 @@ const Canvas = props =>{
     const canvasRef = useRef(null);
 
     const draw = ctx => {
-        ctx.fillStyle = '#000000'
-        ctx.beginPath()
-        ctx.arc(50, 100, 20, 0, 2*Math.PI)
-        ctx.fill()
+        ctx.fillStyle = '#000000';
+        ctx.beginPath();
+        ctx.arc(50, 100, 20, 0, 2*Math.PI);
+        ctx.fill();
       }
     useEffect(() => {
-        const canvas = canvasRef.current
-        const context = canvas.getContext('2d')
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
         //Our first draw
         draw(context);
     }, [])
