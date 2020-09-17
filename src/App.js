c7a1831ae79a92eda3fbc7880cfb7a8e4ce73eae
LaupWing <loc_strict@hotmail.com>
2020-09-17 23:17:25 +0200

    first canvas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Canvas from './Canvas';
+
 
 function App() {
   return (
         <div className="App">
-            
+            <Canvas/>
         </div>
   );
 }
