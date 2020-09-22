97bfad1f43a78f67429417c5aa7bc4df4a23b522
LaupWing <loc_strict@hotmail.com>
2020-09-22 12:44:53 +0200

    button

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../components/Logo/LoginLogo';
 import styles from './Login.module.css';
 import TextInput from '../components/UI/Inputs/Text/Text'
+import Button from '../components/UI/Button/Button'
 
 const Login = () =>{
     return (
@@ -10,6 +11,9 @@ const Login = () =>{
             <h2>Login</h2>
             <TextInput placeholder="Email"/>
             <TextInput placeholder="Password"/>
+            <Button
+                content="Login"
+            />
         </form>
     );
 }
