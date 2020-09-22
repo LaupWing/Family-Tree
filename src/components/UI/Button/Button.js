7fe16c842f307e7ed0740392d3997fd968440ba9
LaupWing <loc_strict@hotmail.com>
2020-09-22 13:20:00 +0200

    base styling

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import styles from './Button.module.css'
+import styles from './Button.module.css';
 
-const Button = ({cls, content, styling}) => {
-    return (
-        <button 
-            style={{...styling}} 
-            className={`${styles.button} ${styles[cls]}`}
-        >
-            {content}
-        </button>
-    );
-}
+const Button = ({ cls, content, styling }) => {
+	return (
+		<button
+			style={{ ...styling }}
+			className={`${styles.button} ${styles[cls]}`}
+		>
+			{content}
+		</button>
+	);
+};
 
 export default Button;
