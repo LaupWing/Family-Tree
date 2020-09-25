85b202eec1ca33d7301c2e52035cee75f1366acb
LaupWing <loc_strict@hotmail.com>
2020-09-25 10:33:04 +0200

    social btns

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -4,6 +4,8 @@ import styles from './Login.module.css';
 import TextInput from '../components/UI/Inputs/Text/Text';
 import Button from '../components/UI/Button/Button';
 import { Link } from 'react-router-dom';
+const google = require('../assets/images/google.png');
+const facebook = require('../assets/images/facebook.png');
 
 const Login = () => {
 	return (
@@ -16,10 +18,14 @@ const Login = () => {
 				<i>Forgot my password</i>
 			</Link>
 			<Button type="submit" content="Login" />
-			<p>or</p>
+			<p>or login with</p>
 			<div className={styles.buttons}>
-				<button></button>
-				<button></button>
+				<button type="button">
+					<img src={facebook} />
+				</button>
+				<button type="button">
+					<img src={google} />
+				</button>
 			</div>
 		</form>
 	);
