795d324b657a5cef5f9919b0fdada8be402c9f0c
LaupWing <loc_strict@hotmail.com>
2020-10-28 18:43:13 +0100

    Logout

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,9 +1,11 @@
 import Chats from './Chats/Chats';
 import Tree from './Tree/Tree';
 import Friends from './Friends/Friends';
+import Logout from './Logout/Logout';
 
 export default {
    Chats,
    Tree,
-   Friends
+   Friends,
+   Logout
 }
