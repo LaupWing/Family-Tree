e66f6feffc09f54506ebc110c116a42ca9fabcba
LaupWing <loc_strict@hotmail.com>
2020-10-31 09:55:38 +0100

    canvasref

--- src/views/Home/Canvas/Canvas.js
+++ src/views/Home/Canvas/Canvas.js
@@ -1,16 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import styles from './Canvas.module.css';
 import Shape from './Shape/Shape';
 
 const Canvas = ({active}) => {
    const [start, setStart] = useState(false);
    const [moving, setMoving] = useState(false);
+   const canvasRef = useRef(null);
 
    return (
       <>
-         {(start && moving) && <Shape/>}
+         {(start && moving) && 
+            <Shape
+               start={start}
+               moving={moving}
+               active={active}
+               canvasRef={canvasRef}
+            />
+         }
          <canvas 
             id="canvas" 
+            ref={canvasRef}
             className={styles.canvas}
             onMouseDown={(e)=>{
                if(!start){
--- src/views/Home/Canvas/Shape/Shape.js
+++ src/views/Home/Canvas/Shape/Shape.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './Shape.module.css';
 
-const Shape = () => {
+const Shape = ({active, moving, start, canvasRef}) => {
    return (
       <div className={styles.shape}>
          
       </div>
-   )
+   );
 }
 
-export default Shape
+export default Shape;
