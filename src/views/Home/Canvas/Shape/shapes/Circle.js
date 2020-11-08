fb78cb122a66e8f58f382b3740fd6133ddc92ae5
LaupWing <loc_strict@hotmail.com>
2020-11-08 11:32:12 +0100

    wokring circle

--- src/views/Home/Canvas/Shape/shapes/Circle.js
+++ src/views/Home/Canvas/Shape/shapes/Circle.js
@@ -1,4 +1,4 @@
-export default class Rect{
+export default class Circle{
    constructor(ctx, x, y, dimension){
       this.ctx = ctx;
       this.x = x;
@@ -7,7 +7,13 @@ export default class Rect{
    }
    draw(){
       this.ctx.beginPath();
-      this.ctx.rect(this.x, this.y, this.dimension, this.dimension);
+      this.ctx.arc(
+         this.x + this.dimension / 2, 
+         this.y + this.dimension / 2, 
+         this.dimension / 2, 
+         0,
+         2 * Math.PI
+      );
       this.ctx.stroke();
    }
 }
--- src/views/Home/Canvas/Shape/shapes/index.js
+++ src/views/Home/Canvas/Shape/shapes/index.js
@@ -1,5 +1,7 @@
 import Rect from './Rect.js';
+import Circle from './Circle.js';
 
 export default {
-   Rect
+   Rect,
+   Circle
 }
