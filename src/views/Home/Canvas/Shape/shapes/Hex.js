602fe6e7b1a8597b26f5f92c0ad6a01403a7af1c
LaupWing <loc_strict@hotmail.com>
2020-11-08 16:32:08 +0100

    working hexagon

--- src/views/Home/Canvas/Shape/shapes/Hex.js
+++ src/views/Home/Canvas/Shape/shapes/Hex.js
@@ -7,17 +7,19 @@ export default class Hex{
    }
    draw(){
       this.ctx.beginPath();
-      for (let side = 0; side < 7; side++) {
-         this.ctx.lineTo(
-            (this.x + (this.size/2)) + 
-            (this.size /2) * 
-            Math.cos(side * 2 * Math.PI / 6), 
-            (this.y + (this.size/2)) + 
-            (this.size /2) 
-            * Math.sin(side * 2 * Math.PI / 6)
-         );
-      }
+      let angle = 0;
 
+      for (let i = 0; i <= 6; i++) {
+         angle = (2 * Math.PI) / 6 * (i + 0.5); 
+         const x_i = (this.x + (this.size/2)) + (this.size/2) * Math.cos(angle);
+         const y_i = (this.y + (this.size/2)) + (this.size/2) * Math.sin(angle);
+         if (i === 0) {
+            this.ctx.moveTo(x_i, y_i);
+         } else {
+            this.ctx.lineTo(x_i, y_i); 
+         }
+      
+      }
       this.ctx.stroke();
    }
 }
