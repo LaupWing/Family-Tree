8867eb6b4709c9ab4788a5845ae2dde73eb12a36
LaupWing <loc_strict@hotmail.com>
2020-11-15 19:22:52 +0100

    added snapshots

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,2 +1,4 @@
 export const SET_USER = 'SET_USER';
 export const SET_SHAPES = 'SET_SHAPES';
+export const SET_SNAPSHOTS = 'SET_SNAPSHOTS';
+export const SET_SNAPSHOT = 'SET_SNAPSHOT';
--- src/store/reducers/shapes.js
+++ src/store/reducers/shapes.js
@@ -3,6 +3,7 @@ import * as actionTypes from '../actions/actionTypes';
 const initialState ={
 	shapes: [],
 	snapshots: [],
+	snapshot: [],
 }
 
 const setUser = (state, shapes)=>{
@@ -11,10 +12,24 @@ const setUser = (state, shapes)=>{
       shapes
 	}
 }
+const setSnapshots = (state, snapshots)=>{
+	return {
+      ...state,
+      snapshots
+	}
+}
+const setSnapshot = (state, snapshot)=>{
+	return {
+      ...state,
+      snapshot
+	}
+}
 
 const reducer = (state=initialState, action)=>{
 	switch(action.type){
 		case actionTypes.SET_SHAPES: return setUser(state, action.shapes);
+		case actionTypes.SET_SNAPSHOTS: return setSnapshots(state, action.snapshots);
+		case actionTypes.SET_SNAPSHOT: return setSnapshot(state, action.snapshot);
 		default:
 			return state;
 	}
